3bf8841d05eda17e9d76791b621cf2046c4b3a0a
Leon Walter <l.d.walter@freenet.de>
2023-10-14 15:14:40 +0200

    Export to XML & JSON, including tests

--- src/main/kotlin/de/hs_fl/sbg/plugins/datafile_converter/converter/to/ConverterToJSON.kt
+++ src/main/kotlin/de/hs_fl/sbg/plugins/datafile_converter/converter/to/ConverterToJSON.kt
@@ -1,10 +1,53 @@
 package de.hs_fl.sbg.plugins.datafile_converter.converter.to
 
+import de.hs_fl.sbg.plugins.datafile_converter.converter.internal.Node
 import de.hs_fl.sbg.plugins.datafile_converter.converter.internal.Tree
+import kotlinx.serialization.json.*
+import java.io.File
 
-class ConverterToJSON: IConvertTo {
+class ConverterToJSON : IConvertTo {
     override fun convertAndWrite(tree: Tree, pathFileName: String) {
-        TODO("Not yet implemented")
+        val outputFile = File("$pathFileName.json")
+
+        val json = buildJsonObject { nodeAsJSON(tree.root, this) }
+
+        if (!outputFile.exists()) outputFile.createNewFile()
+        outputFile.writeText(json.toString())
     }
 
+    private fun nodeAsJSON(curNode: Node, json: JsonObjectBuilder): JsonObjectBuilder {
+        json.putJsonObject(curNode.name) {
+            val properties = curNode.getProperties()
+            val isPropertiesEmpty = properties.isEmpty()
+            val children = curNode.getChildren()
+            val isChildrenEmpty = children.isEmpty()
+
+            //properties
+            for (pair in properties) {
+                val key = pair.key
+                when (val value = pair.value) {
+                    is Int -> put(key, value)
+                    is Long -> put(key, value)
+                    is Float -> put(key, value)
+                    is Double -> put(key, value)
+                    is Boolean -> put(key, value)
+                    is String -> put(key, value)
+                    else -> put(key, value.toString())
+                }
+            }
+
+            if (isChildrenEmpty && isPropertiesEmpty) {
+                // Empty children array, in case the object has no properties and no children, for whatever reason
+                this.putJsonArray("children") {}
+            } else if (!isChildrenEmpty) {
+                //children
+                this.putJsonArray("children") {
+                    for (child in children) {
+                        addJsonObject { nodeAsJSON(child, this) }
+                    }
+                }
+            }
+        }
+        return json
+    }
 }
--- src/main/kotlin/de/hs_fl/sbg/plugins/datafile_converter/converter/to/ConverterToXML.kt
+++ src/main/kotlin/de/hs_fl/sbg/plugins/datafile_converter/converter/to/ConverterToXML.kt
@@ -1,10 +1,56 @@
 package de.hs_fl.sbg.plugins.datafile_converter.converter.to
 
+import de.hs_fl.sbg.plugins.datafile_converter.converter.internal.Node
 import de.hs_fl.sbg.plugins.datafile_converter.converter.internal.Tree
+import java.io.BufferedWriter
+import java.io.File
 
 class ConverterToXML : IConvertTo {
     override fun convertAndWrite(tree: Tree, pathFileName: String) {
-        TODO("Not yet implemented")
+        val outputFile = File("$pathFileName.xml")
+
+        if (!outputFile.exists()) outputFile.createNewFile()
+        val writer = outputFile.bufferedWriter()
+        writer.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>")
+        writer.newLine()
+
+        nodeToXML(tree.root, writer)
+
+        writer.close()
+    }
+
+    private fun nodeToXML(curNode: Node, writer: BufferedWriter) {
+        val properties = curNode.getProperties()
+        val isPropertiesEmpty = properties.isEmpty()
+        val children = curNode.getChildren()
+        val isChildrenEmpty = children.isEmpty()
+        val name = curNode.name
+
+        if (isChildrenEmpty && isPropertiesEmpty) {
+            writer.appendLine("<$name/>")
+        } else if(!isChildrenEmpty) {
+            if (isPropertiesEmpty)
+                writer.appendLine("<$name>")
+            else {
+                writer.append("<$name")
+                for (pair in properties) {
+                    writer.append(" ${pair.key}=\"${pair.value}\"")
+                }
+                writer.appendLine(">")
+            }
+            
+            for (child in children) {
+                nodeToXML(child, writer)
+            }
+
+            writer.appendLine("</$name>")
+        } else {
+            writer.append("<$name")
+            for (pair in properties) {
+                writer.append(" ${pair.key}=\"${pair.value}\"")
+            }
+            writer.appendLine("/>")
+        }
     }
 
 }
