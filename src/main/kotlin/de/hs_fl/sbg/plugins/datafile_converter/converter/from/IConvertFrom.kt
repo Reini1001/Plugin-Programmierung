92248e308e4efb77a6bb26cdadc9fee12d81715c
Reini1001 <58426111+Reini1001@users.noreply.github.com>
2023-10-23 10:09:13 +0200

    KDoc Strings V2

--- src/main/kotlin/de/hs_fl/sbg/plugins/datafile_converter/converter/from/IConvertFrom.kt
+++ src/main/kotlin/de/hs_fl/sbg/plugins/datafile_converter/converter/from/IConvertFrom.kt
@@ -6,12 +6,16 @@ import java.io.File
 interface IConvertFrom {
 
     /**
+     * Reads [File] from path and return it
+     *
      * @param path The Path to the [File] to be converted
      * @return File for the given path
      */
     fun readFile(path: String): File
 
     /**
+     * Converts [File] to [Tree]
+     *
      * @param file [File] to be converted
      * @return [Tree] created from [File] content
      */
